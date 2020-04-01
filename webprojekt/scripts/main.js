9872da8759617854aab8ce23269a616263f7a904
afkfabbot <63010387+afkfabbot@users.noreply.github.com>
2020-04-01 18:55:57 +0200

    Add files via upload

---
+++ webprojekt/scripts/main.js
@@ -0,0 +1,31 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function(){
+  var mySrc = myImage.getAttribute('src');
+    if (mySrc === 'bilder/marc.jpg'){
+      myImage.setAttribute('src', 'bilder/spagetti.jpg');
+    }else{
+      myImage.setAttribute('src','bilder/marc.jpg');
+    }
+}
+
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Bitte geben Sie Ihren Namen ein.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla ist cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla ist cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
